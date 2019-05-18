b5229aaf0c4c3d2cd03eb4c0d999692b91843d20
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2019-05-18 05:10:06 +0300

    Added inner call system and json

--- core.py
+++ core.py
@@ -1,38 +1,81 @@
-import useragents
-
+from useragents import user_agent_list
 import scrapy
 import random
+import json
+import atexit
 
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
 from twisted.internet.error import TimeoutError, TCPTimedOutError
 
+recipes = {
+    "yemekler": []
+}
+prefix = "https://www.nefisyemektarifleri.com/kategori/tarifler/"
+MAX_PAGE = 2
+
+out = open("hede.json", "w")
+
+
+def createJSON():
+    print(json.dumps(recipes, ensure_ascii=False), file=out)
+
 
 class ErrbackSpider(scrapy.Spider):
     name = "errback_example"
     start_urls = [
-        "https://www.nefisyemektarifleri.com/kremali-domates-corbasi-5922589/"
+        "corba-tarifleri",
+        "kahvaltilik-tarifleri"
     ]
+
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 1
+    }
 
     def start_requests(self):
         for u in self.start_urls:
-            yield scrapy.Request(u, callback=self.parse_httpbin,
-                                    errback=self.errback_httpbin,
-                                    dont_filter=True,
-                                    headers={'User-Agent' : 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'})
+            for page in range(1, MAX_PAGE):
+                url = prefix + u + "/page/" + str(page) + "/"
+                user_agent = random.choice(user_agent_list)
+                yield scrapy.Request(url, callback=self.parse_httpbin,
+                                     errback=self.errback_httpbin,
+                                     dont_filter=True,
+                                     headers={'User-Agent': user_agent})
 
     def parse_httpbin(self, response):
-        self.logger.info('Got successful response from {}'.format(response.url))
-        selectorStuff = response.selector.xpath('//div[@class="entry_content tagoninread"]/ul/li[@itemprop="ingredients"]/text()').getall()
-        selectorIngredients = response.selector.xpath('//div[@class="entry_content tagoninread"]/ol/li[@itemprop="ingredients"]/text()').getall()
+        self.logger.info(
+            'Got successful response from {}'.format(response.url))
+        foodsOnPage = response.selector.xpath(
+            '//div[@class="post-img-div"]/a/@href').getall()
+        for foodUrl in foodsOnPage:
+            user_agent = random.choice(user_agent_list)
+            yield scrapy.Request(foodUrl, callback=self.parse_httpbinfood,
+                                 errback=self.errback_httpbin,
+                                 dont_filter=True,
+                                 headers={'User-Agent': user_agent})
 
-        return {selectorStuff, selectorIngredients}
-
+    def parse_httpbinfood(self, response):
+        name = response.selector.xpath('//h1[@itemprop="name"]/text()').get()
+        selectorIngredients = response.selector.xpath(
+            '//div[@class="entry_content tagoninread"]/ul/li[@itemprop="ingredients"]/text()').getall()
+        selectorRecipe = response.selector.xpath(
+            '//div[@class="entry_content tagoninread"]/ol/li[@itemprop="ingredients"]/text()').getall()
+        portion = response.selector.xpath(
+            '//span[@itemprop="recipeYield"]/strong/text()').get()
+        times = response.selector.xpath(
+            '//div[@class="tarif_meta_box"]/span/strong/text()').getall()
+        recipes["yemekler"].append({
+            "isim": name,
+            "malzemeler": selectorIngredients,
+            "adimlar": selectorRecipe,
+            "kisilik": portion,
+            "hazirlama": times[0],
+            "pisirme": times[1]
+        })
 
     def errback_httpbin(self, failure):
         # log all failures
         self.logger.error(repr(failure))
-
 
         if failure.check(HttpError):
             response = failure.value.response
@@ -46,3 +89,6 @@ class ErrbackSpider(scrapy.Spider):
         elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
             self.logger.error('TimeoutError on %s', request.url)
+
+
+atexit.register(createJSON)
